40d5b0c778e20de47d68e1e6471849ce429e773b
Andrew Zhu <mommothazaz123@gmail.com>
2018-01-13 00:13:36 -0800

    update script

--- static/rr.js
+++ static/rr.js
@@ -1,1 +1,4 @@
-window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+function codeAddress() {
+            window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+window.onload = codeAddress;
