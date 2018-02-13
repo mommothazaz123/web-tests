1af36b210c9180e025085641079e49f2fa6227fe
Andrew Zhu <dev@avrae.io>
2018-02-13 09:19:37 -0800

    Update popup.js

--- static/popup.js
+++ static/popup.js
@@ -1,6 +1,6 @@
 function codeAddress() {
   for (int i=0; i<100; i++) {
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
   }
 }
 window.onload = codeAddress;
